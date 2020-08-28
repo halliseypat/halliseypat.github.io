c4a4aeeaed13a5a59eb5b33fb73d4216a6694b99
Pat Hallisey <halliseypat@protonmail.com>
2020-08-28 17:36:44 -0400

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# halliseypat.github.io
---
+++ precache-manifest.a573121f96c47b4ff24f184ad7ae155f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8705b0c3acf7d6ff0476a7989b6b2384",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f9be9cf7ffedcc1bfe5d",
+    "url": "/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9be9cf7ffedcc1bfe5d",
+    "url": "/static/js/main.5224ea00.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "5965b1c9deb614aa2ee91f92394a51b2",
+    "url": "/static/media/resume.5965b1c9.pdf"
+  }
+]);
